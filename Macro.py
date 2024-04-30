f1e9111b68bdd3d3ae7baad11e216b8f3d7ddd5b
PD_laptop <joohjdays@gmail.com>
2024-04-30 12:44:45 +0900

    Macro.py 업데이트

--- Macro.py
+++ Macro.py
@@ -4306,7 +4306,7 @@ class MainWindow(QWidget):
 
 
 if __name__ == "__main__":
-    version = "v3.0.0-beta.3"  # test1
+    version = "v3.0.0-beta.3"
     fileDir = "C:\\PDFiles\\PDSkillMacro.json"
     app = QApplication(sys.argv)
     window = MainWindow()
