346f20831eda6f9c9e834468c95d95f6357e1ebd
프됒 <57057173+Pro-Days@users.noreply.github.com>
2025-01-18 22:42:17 +0900

    Delete pyinstaller directory

--- pyinstaller/bootloader/build/c4che/build.config.py
+++
@@ -1,2 +0,0 @@
-version = 0x2001400
-tools = [{'tool': 'winres', 'tooldir': None, 'funs': None}, {'tool': 'c', 'tooldir': None, 'funs': None}, {'tool': 'cxx', 'tooldir': None, 'funs': None}, {'tool': 'msvc', 'tooldir': None, 'funs': None}, {'tool': 'compiler_c', 'tooldir': None, 'funs': None}]
--- pyinstaller/bootloader/build/waf-print-msvc-1.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set INCLUDE=
-set LIB=
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-echo PATH=%PATH%
-echo INCLUDE=%INCLUDE%
-echo LIB=%LIB%;%LIBPATH%
