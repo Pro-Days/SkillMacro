7cb649bd128d3982692494ec570731cbb9e770d5
PD_Desktop <joohjdays@gmail.com>
2026-03-28 03:00:36 +0900

    버전업

--- app/scripts/config.py
+++ app/scripts/config.py
@@ -77,7 +77,7 @@ class MacroSpecs:
 
 @dataclass(frozen=True)
 class AppConfig:
-    version: ClassVar[str] = "v1.0.0"
+    version: ClassVar[str] = "v1.0.1"
     app_name: ClassVar[str] = "PD SkillMacro"
 
     ui: ClassVar[UiConfig] = UiConfig()
